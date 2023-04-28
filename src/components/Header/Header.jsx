cfe61945e95e5701588c3b4fbfa225801bf40c27
unknown <apurbahasanj@gmail.com>
2023-04-28 15:16:58 +0600

    done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,6 +3,7 @@ import "./Header.css";
 import logo from "../../images/Logo.svg";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../Providers/AuthProvider";
+import { FaUserAlt } from 'react-icons/fa';
 import { toast } from "react-hot-toast";
 
 const Header = () => {
@@ -22,15 +23,16 @@ const Header = () => {
     <div className="w-full py-6 header  bg-black  ">
       <nav className="flex max-w-7xl items-center justify-center mx-auto ">
         <img src={logo} alt="" />
-        <div className="ml-auto">
+        <div className="ml-auto flex items-center ">
           <Link to="/">Shop</Link>
           <Link to="/orders">Order Review</Link>
           <Link to="/inventory">Manage Inventory</Link>
           <Link to="/signup">Sign Up</Link>
-          <Link to="/login">Login</Link>
+          {user ? "" : <Link to="/login">Login</Link>}
           {user && (
-            <span className="text-orange-300">
-              welcome{user.email}{" "}
+            <span className=" ml-5 flex items-center gap-4">
+              {/* welcome{user.email}{" "} */}
+              <FaUserAlt className="text-4xl text-secondary hover:text-orange-300"></FaUserAlt>
               <button
                 onClick={handleLogOut}
                 className="btn bg-secondary capitalize text-xl border-none hover:bg-orange-300 text-black"
--- src/components/Inventory/Inventory.jsx
+++ src/components/Inventory/Inventory.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 
 const Inventory = () => {
-    return (
-        <div>
-            Inventory
-        </div>
-    );
+  return (
+    <div className='min-h-screen flex items-center justify-center'>
+      <h1 className='text-7xl text-center'>
+        <span className='text-orange-500 pr-5 border-r-4 border-orange-500'>404</span>
+        <span>Nothing here to see</span>
+      </h1>
+    </div>
+  );
 };
 
 export default Inventory;
